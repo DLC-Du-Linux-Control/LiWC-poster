4669a72826ea5d061745653981953f3198d02dec
jiho <jsong9401@gmail.com>
2024-10-10 19:19:57 +0900

    origin

---
+++ src/main/java/com/dlc/linux_web_controller/LinuxWebControllerApplication.java
@@ -0,0 +1,13 @@
+package com.dlc.linux_web_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxWebControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinuxWebControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlc/linux_web_controller/LinuxWebControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlc.linux_web_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxWebControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
